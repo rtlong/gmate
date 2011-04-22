b3138ba0b10f5e6de9dd6111f3911148516d7daf
Ryan Taylor Long <ryan@rtlong.com>
2011-04-22 05:25:41 -0500

    modified install.sh to suit my needs

--- install.sh
+++ install.sh
@@ -6,15 +6,15 @@
 #echo "Type root password if you want to install system wide. Press [Enter] to install to this user only"
 #sudo -v
 
-if [ $(id -u) = "0" ]; then
-  sudo="yes"
-else
+#if [ $(id -u) = "0" ]; then
+#  sudo="yes"
+#else
   sudo="no"
-fi
+#fi
 
 # Make it talk
 alias mkdir="mkdir -v"
-alias cp="cp -vl --no-clobber"
+alias cp="cp --verbose --backup --interactive --suffix=.bak"
 
 # Register rails-related mime types
 if [ $sudo = "yes" ]; then
@@ -32,14 +32,15 @@ else
   cp lang-specs/* ~/.local/share/gtksourceview-2.0/language-specs/
 fi
 
-if [ $sudo = "yes" ]; then
-  # Copy Gmate executable
-  sudo mkdir -p /usr/share/gedit-2/gmate
-  sudo cp gmate.py /usr/share/gedit-2/gmate/gmate.py
-else
-  mkdir -p ~/.bin/
-  cp gmate.py ~/.bin/gmate
-fi
+#if [ $sudo = "yes" ]; then
+#  # Copy Gmate executable
+#  sudo mkdir -p /usr/share/gedit-2/gmate
+#  sudo cp gmate.py /usr/share/gedit-2/gmate/gmate.py
+#else
+#  mkdir -p ~/.bin/
+#  cp gmate.py ~/.bin/gmate
+#fi
+
 # Copy Tags
 if [ $sudo = "yes" ]; then
   sudo mkdir -p /usr/share/gedit-2/plugins/taglist/
@@ -98,27 +99,27 @@ fi
 #  `sh ./debian/postinst`
 #fi
 
-echo -n "Do you want to activate default plugin and configuration set? [y,N]:"
-read answer
-case "$answer" in
-    [yY])
-        `gconftool-2 --set /apps/gedit-2/plugins/active-plugins -t list --list-type=str [rails_extract_partial,rubyonrailsloader,align,smart_indent,text_tools,completion,quickhighlightmode,gemini,trailsave,rails_hotkeys,fuzzyopen,filebrowser,snippets,modelines,smartspaces,docinfo,time,spell,terminal,drawspaces,codecomment,colorpicker,indent,encodingpy,FindInProject]`
-        `gconftool-2 --set /apps/gedit-2/preferences/editor/auto_indent/auto_indent -t bool true`
-        `gconftool-2 --set /apps/gedit-2/preferences/editor/bracket_matching/bracket_matching -t bool true`
-        `gconftool-2 --set /apps/gedit-2/preferences/editor/current_line/highlight_current_line -t bool true`
-        `gconftool-2 --set /apps/gedit-2/preferences/editor/cursor_position/restore_cursor_position -t bool true`
-        `gconftool-2 --set /apps/gedit-2/preferences/editor/line_numbers/display_line_numbers -t bool true`
-        `gconftool-2 --set /apps/gedit-2/preferences/editor/right_margin/display_right_margin -t bool true`
-        `gconftool-2 --set /apps/gedit-2/preferences/editor/right_margin/right_margin_position -t int 80`
-        `gconftool-2 --set /apps/gedit-2/preferences/editor/colors/scheme -t str twilight`
-        `gconftool-2 --set /apps/gedit-2/preferences/editor/tabs/insert_spaces -t bool true`
-        `gconftool-2 --set /apps/gedit-2/preferences/editor/tabs/tabs_size -t int 4`
-        `gconftool-2 --set /apps/gedit-2/preferences/editor/wrap_mode/wrap_mode -t str GTK_WRAP_NONE`
-        `gconftool-2 --set /apps/gedit-2/preferences/editor/save/create_backup_copy -t bool false`
-        echo "Configuration set."
-        ;;
-    *)
-        echo "No config performed."
-        ;;
-esac
+#echo -n "Do you want to activate default plugin and configuration set? [y,N]:"
+#read answer
+#case "$answer" in
+#    [yY])
+#        `gconftool-2 --set /apps/gedit-2/plugins/active-plugins -t list --list-type=str [rails_extract_partial,rubyonrailsloader,align,smart_indent,text_tools,completion,quickhighlightmode,gemini,trailsave,rails_hotkeys,fuzzyopen,filebrowser,snippets,modelines,smartspaces,docinfo,time,spell,terminal,drawspaces,codecomment,colorpicker,indent,encodingpy,FindInProject]`
+#        `gconftool-2 --set /apps/gedit-2/preferences/editor/auto_indent/auto_indent -t bool true`
+#        `gconftool-2 --set /apps/gedit-2/preferences/editor/bracket_matching/bracket_matching -t bool true`
+#        `gconftool-2 --set /apps/gedit-2/preferences/editor/current_line/highlight_current_line -t bool true`
+#        `gconftool-2 --set /apps/gedit-2/preferences/editor/cursor_position/restore_cursor_position -t bool true`
+#        `gconftool-2 --set /apps/gedit-2/preferences/editor/line_numbers/display_line_numbers -t bool true`
+#        `gconftool-2 --set /apps/gedit-2/preferences/editor/right_margin/display_right_margin -t bool true`
+#        `gconftool-2 --set /apps/gedit-2/preferences/editor/right_margin/right_margin_position -t int 80`
+#        `gconftool-2 --set /apps/gedit-2/preferences/editor/colors/scheme -t str twilight`
+#        `gconftool-2 --set /apps/gedit-2/preferences/editor/tabs/insert_spaces -t bool true`
+#        `gconftool-2 --set /apps/gedit-2/preferences/editor/tabs/tabs_size -t int 4`
+#        `gconftool-2 --set /apps/gedit-2/preferences/editor/wrap_mode/wrap_mode -t str GTK_WRAP_NONE`
+#        `gconftool-2 --set /apps/gedit-2/preferences/editor/save/create_backup_copy -t bool false`
+#        echo "Configuration set."
+#        ;;
+#    *)
+#        echo "No config performed."
+#        ;;
+#esac
 
